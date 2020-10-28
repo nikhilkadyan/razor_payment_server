da3c51aab096fe4c9acb63ef8c74c0395a9f0c24
Nikhil Kadyan <nikhil@Nikhils-MacBook-Pro.local>
2020-10-28 15:52:35 +0530

    updated products

--- modules/products.js
+++ modules/products.js
@@ -2,26 +2,10 @@ module.exports = function(){
     let products = [
         {
             id: 1,
-            name: 'Elite SMM Panel Script',
-            amount: 500,
+            name: 'SMM Script Bundle (Elite | MSP | FOX)',
+            amount: 999,
             currency: 'INR',
-            demo: 'http://elitesmm.nikhilkadyan.com',
-            secret: 'https://drive.google.com/file/d/1unv7cK0udzBqtAjftIKn7nyfkzSpdTRw/view?usp=sharing'
-        },
-        {
-            id: 2,
-            name: 'MSP Panel Script',
-            amount: 500,
-            currency: 'INR',
-            demo: 'https://nikhilkadyan.com',
-            secret: 'https://drive.google.com/file/d/1R6h4H6l-Nk98kaAoTFyhHTh1zpuJ3fPR/view?usp=sharing'
-        },
-        {
-            id: 3,
-            name: 'Fox Panel Script',
-            amount: 500,
-            currency: 'INR',
-            demo: 'https://smm.nikhilkadyan.com',
+            demo: 'http://smm.nikhilkadyan.com',
             secret: 'https://drive.google.com/file/d/1y6TEP2QWsZ6gzqpqKrlJdGIT2WWz09_8/view?usp=sharing'
         }
     ];
